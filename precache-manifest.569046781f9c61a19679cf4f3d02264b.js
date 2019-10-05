411c4260f4181fbb0cfb84fb83d444ac97f3cd49
lastshadowpl <xeloxpl@windowslive.com>
2019-10-05 16:39:53 +0200

    Updates

---
+++ precache-manifest.569046781f9c61a19679cf4f3d02264b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed028b426b0c288f14bae5ef70f8254",
+    "url": "/playlister/index.html"
+  },
+  {
+    "revision": "aed7f1f6bc1ea70763a5",
+    "url": "/playlister/static/css/main.5d6aa202.chunk.css"
+  },
+  {
+    "revision": "46ef8f535d09510ca9d8",
+    "url": "/playlister/static/js/2.f8fc5e13.chunk.js"
+  },
+  {
+    "revision": "aed7f1f6bc1ea70763a5",
+    "url": "/playlister/static/js/main.df4a050a.chunk.js"
+  },
+  {
+    "revision": "2deb48582dcbf23e9575",
+    "url": "/playlister/static/js/runtime-main.559b2c19.js"
+  }
+]);
